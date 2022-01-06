33558d1e0d5bbe6aff50545de57bb6082f3a155a
Sunil Babu <msunilbabu@outlook.com>
2022-01-06 15:48:14 +0530

    project

---
+++ EncryptDecrypt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace EncryptDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                byte[] decryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This needs
+                    //to include the private key information.
+
+                    RSAParameters RSAKeyInfo = new RSAParameters();
+                    
+                    
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Decrypt the passed byte array and specify OAEP padding.  
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.  
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+               
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                
+            }
+        }
+    }
+}
