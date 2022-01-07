28f19852aa53c09b0c24af3f9f6a34a95676a86d
Sunil Babu M <mbabusunu@gmail.com>
2022-01-07 12:38:49 +0530

    Update Program.cs

--- EncryptDecrypt/Program.cs
+++ EncryptDecrypt/Program.cs
@@ -13,17 +13,8 @@ namespace EncryptDecrypt
                 //Create a new instance of RSACryptoServiceProvider.
                 using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
                 {
-                    //Import the RSA Key information. This needs
-                    //to include the private key information.
-
                     RSAParameters RSAKeyInfo = new RSAParameters();
-                    
-                    
                     RSA.ImportParameters(RSAKeyInfo);
-
-                    //Decrypt the passed byte array and specify OAEP padding.  
-                    //OAEP padding is only available on Microsoft Windows XP or
-                    //later.  
                     decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
                 }
                
